777ab005b97feb3f3e3a5b0afbc37f2390f9ada7
Your GitHub TIRTH-25 <tirthp2504@gmail.com>
2026-09-01 20:25:15 +0530

    Update changes

--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -1,3 +1,5 @@
+import { RiCheckLine } from "@remixicon/react";
+
 const Membership = () => {
   const plans = [
     {
@@ -43,7 +45,7 @@ const Membership = () => {
     <div className=" bg-[#F8F9FA] px-6 py-20">
       <div className="text-center">
         <h2 className="text-3xl font-bold">
-          Choose Your <span>Membership Plan</span>
+          Choose Your <span className="text-red-500">Membership Plan</span>
         </h2>
         <p className="mt-6 text-gray-500 text-[17px]">
           Select the perfect plan that fits your fitness journey. All plans
@@ -55,10 +57,18 @@ const Membership = () => {
             <div className="px-3 py-3 bg-white">
                 <h3 className="font-bold text-2xl">{plan.name}</h3>
                 <p className="text-gray-500 text-sm">{plan.subtitle}</p>
-                <div className="flex justify-center items-end">
-                    <span>₹</span>
-                    <span>{plan.price}</span>
-                    <span>{plan.duration}</span>
+                <div className="flex justify-center items-end gap-1 mt-5">
+                    <span className="text-xl text-red-500 font-bold">₹</span>
+                    <span className="text-4xl font-bold">{plan.price}</span>
+                    <span className="text-gray-500">{plan.duration}</span>
+                    <div>
+                        {plan.features.map((feature) => {
+                            <div>
+                                <RiCheckLine size={20}className="text-red-500 shrink-0"/>
+                                <p className="text-gray-600">{feature}</p>
+                            </div>
+                        })}
+                    </div>
                 </div>
                 
             </div>
